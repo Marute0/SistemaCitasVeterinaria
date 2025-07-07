9ebd6aedcd8a29123b0d8948312a8f616f3b2126
Ususario <Ususario@MSI>
2025-07-07 10:41:47 -0500

    Agregar paquetes de controlador y vista

--- src/main/java/Modelo/Administrador.java
+++ src/main/java/Modelo/Administrador.java
@@ -1,41 +1,39 @@
-package com.mycompany.sistemacitasveterinarias.Model;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package Modelo;
+
+
 
 /**
  *
  * @author Ususario
  */
-public class DoctorVeterinario extends Persona{
+public class Administrador extends Persona{
     //Atributos
-    private String usuario;
-    private String contraseña;
-    private String especializacion;
-    private String idEmpresarial;
+        private String usuario;
+        private String contraseña;
 
-     //Constructor
+    //Constructor
 
-    public DoctorVeterinario(String usuario, String contraseña, String especializacion, String idEmpresarial, String nombre, String apellido, String id, String email, String numeroTelefono) {
+    public Administrador(String usuario, String contraseña, String nombre, String apellido, String id, String email, String numeroTelefono) {
         super(nombre, apellido, id, email, numeroTelefono);
         this.usuario = usuario;
         this.contraseña = contraseña;
-        this.especializacion = especializacion;
-        this.idEmpresarial = idEmpresarial;
     }
-   
+    
 
     //Getters
-    public String getEspecializacion() {
-        return especializacion;
+    public String getUsuario() {
+        return usuario;
     }
 
-    public String getIdEmpresarial() {
-        return idEmpresarial;
+    public String getContraseña() {
+        return contraseña;
     }
 
+    
     //Setters
     public void setUsuario(String usuario) {
         this.usuario = usuario;
@@ -44,20 +42,19 @@ public class DoctorVeterinario extends Persona{
     public void setContraseña(String contraseña) {
         this.contraseña = contraseña;
     }
-
     
     
+    //Método para posterior guargado en la base de datos
     @Override
     public String mostrarInformación() {
         return getNombre() + " " + 
                getApellido() + "," + 
-               usuario + "," + 
-               contraseña + "," + 
                getId() + "," + 
                getEmail() + "," + 
                getNumeroTelefono() + "," + 
-               idEmpresarial + "," + 
-               especializacion + ",";
+               usuario + "," +
+               contraseña;
     }
-
+        
+        
 }
