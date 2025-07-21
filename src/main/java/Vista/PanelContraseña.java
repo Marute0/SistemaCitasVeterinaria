818e925713c64ac5cd155d3904967bbcc6dd6e76
Daniela <Daniela@DESKTOP-AVL16SL>
2025-07-21 09:19:56 -0500

    no sirvio

---
+++ src/main/java/Vista/PanelContraseña.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelContraseña extends JPanel {
+    public PanelContraseña(){
+        setLayout(new BorderLayout());
+        JLabel label = new JLabel("Aquí va la gestión de contraseña", SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+}
+}
---
+++ src/main/java/Vista/PanelDoctores.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Vista;
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelDoctores extends JPanel{
+    public PanelDoctores(){
+        setLayout(new BorderLayout());
+        JLabel label = new JLabel("Aquí va la gestión de doctores", SwingConstants.CENTER);
+        add(label, BorderLayout.CENTER);
+}
+}
